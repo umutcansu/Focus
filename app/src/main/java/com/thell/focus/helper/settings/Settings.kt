d37cc07cc308d9f2b7b85b7e4b29aa962cc83065
Umut Cansu <umutcansugmail.com>
2020-05-03 18:02:33 +0300

    settings database

--- app/src/main/java/com/thell/focus/helper/settings/Settings.kt
+++ app/src/main/java/com/thell/focus/helper/settings/Settings.kt
@@ -2,11 +2,21 @@ package com.thell.focus.helper.settings
 
 object Settings {
 
-    object StateType{
+    object StateType
+    {
         const val NA = -1
         const val NOK = 0
         const val OK = 1
     }
 
-
+    class Key private constructor()
+    {
+        companion object
+        {
+            const val   SETTINGS_KEY_IS_NOTIFICATION_SAVED_ALWAYS =
+                "Would you like to save notifications to history  while notifications are allowed?\nWarning : it may drain your phone battery."
+            const val   SETTINGS_KEY_IS_MUTE_NOTIFICATION_TOAST =
+                "Would you like to receive toast messages?"
+        }
+    }
 }
--- app/src/main/java/com/thell/focus/helper/settings/SettingsHelper.kt
+++ app/src/main/java/com/thell/focus/helper/settings/SettingsHelper.kt
@@ -1,6 +1,21 @@
 package com.thell.focus.helper.settings
 
-class SettingsHelper {
+import com.thell.focus.database.entity.SettingsEntity
 
+class SettingsHelper
+{
+    companion object
+    {
+        val savedAlways = SettingsEntity(1,
+            Settings.Key.Companion::SETTINGS_KEY_IS_NOTIFICATION_SAVED_ALWAYS.name,
+            Settings.Key.SETTINGS_KEY_IS_NOTIFICATION_SAVED_ALWAYS,
+            Settings.StateType.NA
+        )
 
+        val toastMessage = SettingsEntity(2,
+            Settings.Key.Companion::SETTINGS_KEY_IS_MUTE_NOTIFICATION_TOAST.name,
+            Settings.Key.SETTINGS_KEY_IS_MUTE_NOTIFICATION_TOAST,
+            Settings.StateType.OK
+        )
+    }
 }
