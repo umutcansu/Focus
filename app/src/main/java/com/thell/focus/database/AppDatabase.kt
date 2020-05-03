0a336ab88117355c540c70d3250f57f27548f0ec
Umut Cansu <umutcansugmail.com>
2020-05-03 18:03:18 +0300

    room database created

---
+++ app/src/main/java/com/thell/focus/database/AppDatabase.kt
@@ -0,0 +1,57 @@
+package com.thell.focus.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.thell.focus.database.dao.SettingsDao
+import com.thell.focus.database.entity.SettingsEntity
+import com.thell.focus.helper.global.GlobalHelper
+import com.thell.focus.helper.settings.SettingsHelper
+
+@Database(entities = [SettingsEntity::class],version = GlobalHelper.DATABASE_VERSION)
+abstract class AppDatabase : RoomDatabase()
+{
+
+    abstract fun getSettingsDao():SettingsDao
+
+    companion object
+    {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase
+        {
+            return INSTANCE ?:
+            Room.databaseBuilder(context, AppDatabase::class.java,GlobalHelper.DATABASE_NAME).
+            allowMainThreadQueries().
+            fallbackToDestructiveMigration().
+            addCallback(roomCallback).
+            build()
+        }
+
+        private val roomCallback=object: RoomDatabase.Callback()
+        {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                db.execSQL(seedQuery)
+            }
+
+        }
+
+        private val seedQuery = """"
+            insert into Settings 
+            values
+            (
+                |${SettingsHelper.savedAlways.SettingsKey}
+                |${SettingsHelper.savedAlways.SettingsDescription}
+                |${SettingsHelper.savedAlways.State}
+            |,
+                |${SettingsHelper.toastMessage.SettingsKey}
+                |${SettingsHelper.toastMessage.SettingsDescription}
+                |${SettingsHelper.toastMessage.State}
+            )
+            |""".trimMargin()
+    }
+
+}
---
+++ app/src/main/java/com/thell/focus/helper/global/GlobalHelper.kt
@@ -0,0 +1,10 @@
+package com.thell.focus.helper.global
+
+import com.thell.focus.database.entity.SettingsEntity
+
+object GlobalHelper
+{
+    const val   VERSION = "0.0.1"
+    const val   DATABASE_NAME = "FocusDatabase"
+    const val   DATABASE_VERSION = 1
+}
