e18517d92fca64103091d752a7d13a1f30c2e0f8
Umut Cansu <umutcansugmail.com>
2020-05-03 18:19:36 +0300

    settings repository

--- app/src/main/java/com/thell/focus/repository/repo/SettingsRepository.kt
+++ app/src/main/java/com/thell/focus/repository/repo/SettingsRepository.kt
@@ -1,4 +1,30 @@
 package com.thell.focus.repository.repo
 
-class SettingsRepository {
+import androidx.lifecycle.LiveData
+import com.thell.focus.database.entity.SettingsEntity
+import com.thell.focus.repository.util.BaseRepository
+
+class SettingsRepository:BaseRepository<SettingsEntity>()
+{
+    val dao = database.getSettingsDao()
+
+    override fun insert(t: SettingsEntity)
+    {
+        dao.insert(t)
+    }
+
+    override fun delete(t: SettingsEntity)
+    {
+        dao.delete(t)
+    }
+
+    override fun update(t: SettingsEntity)
+    {
+        dao.update(t)
+    }
+
+    override fun getAll(): LiveData<List<SettingsEntity>>
+    {
+        return dao.getAll()
+    }
 }
