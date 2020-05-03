bf0f231ded01d4186cee609325c559127098d7e9
Umut Cansu <umutcansugmail.com>
2020-05-03 18:55:33 +0300

    settings adapter

--- app/src/main/java/com/thell/focus/adapter/SettingsAdapter.kt
+++ app/src/main/java/com/thell/focus/adapter/SettingsAdapter.kt
@@ -8,26 +8,25 @@ import androidx.recyclerview.widget.RecyclerView
 import com.thell.focus.database.entity.SettingsEntity
 import com.thell.focus.databinding.SettingsItemLayoutBinding
 
-class SettingsAdapter(val context: Context, val data:List<SettingsEntity>, val clickListener : (current:SettingsEntity) -> Unit = {}) :
-RecyclerView.Adapter<SettingsAdapter.ViewHolder>()
+class SettingsAdapter(private val context: Context,
+                      private val data:List<SettingsEntity>,
+                      private val clickListener : (current:SettingsEntity) -> Unit = {})
+    : RecyclerView.Adapter<SettingsAdapter.ViewHolder>()
 {
 
     private lateinit var binding: SettingsItemLayoutBinding
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-      /*  val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.settings_item_layout,parent,false)*/
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+    {
         binding = SettingsItemLayoutBinding.inflate(LayoutInflater.from(context))
         val view = binding.root
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return data.size
-    }
+    override fun getItemCount(): Int =data.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    {
         holder.setData(data[position])
     }
 
@@ -40,7 +39,7 @@ RecyclerView.Adapter<SettingsAdapter.ViewHolder>()
 
         fun clickBehavior(item: SettingsEntity)
         {
-
+            
         }
     }
 }
