809f47c34eb1e502f7acbfd7f962f7324a5ba51b
Umut Cansu <umutcansugmail.com>
2020-05-03 16:37:52 +0300

    room api inital

---
+++ app/src/main/java/com/thell/focus/database/util/IDatabaseOperation.kt
@@ -0,0 +1,20 @@
+package com.thell.focus.database.util
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+
+interface IDatabaseOperation<T>
+{
+    @Insert
+    fun insert(t :T)
+
+    @Delete
+    fun delete(t :T)
+
+    @Update
+    fun update(t :T)
+
+    fun getAll(): LiveData<List<T>>
+}
