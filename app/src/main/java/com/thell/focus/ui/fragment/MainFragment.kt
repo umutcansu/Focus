bbb33b203cfd99096a388dcba498cf051c458687
Umut Cansu <umutcansugmail.com>
2020-05-17 16:18:00 +0300

    main fragment

--- app/src/main/java/com/thell/focus/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/thell/focus/ui/fragment/MainFragment.kt
@@ -1,17 +1,27 @@
 package com.thell.focus.ui.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.text.Spanned
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.thell.focus.R
-import com.thell.focus.databinding.ActivityMainBinding
-import com.thell.focus.databinding.FragmentContainerBinding
+import com.thell.focus.broadcastreceiver.NotificationServiceBroadcastReceiver
 import com.thell.focus.databinding.FragmentMainBinding
+import com.thell.focus.helper.bootreceiver.BroadcastReceiverHelper
 import com.thell.focus.helper.global.GuiHelper
+import com.thell.focus.helper.mutestate.IMuteStateAction
+import com.thell.focus.helper.mutestate.MuteStateActionHelper
+import com.thell.focus.helper.mutestate.MuteStateSharedPrefAction
 import com.thell.focus.helper.navigation.IFragmentCallback
 import com.thell.focus.helper.navigation.NavigationMenuHelper
 
@@ -20,6 +30,7 @@ class MainFragment : Fragment() {
 
     private lateinit var navController: NavController
     private  var binding: FragmentMainBinding? = null
+    private lateinit var muteStateAction : IMuteStateAction
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
@@ -30,6 +41,11 @@ class MainFragment : Fragment() {
        return binding?.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        setStateInit()
+    }
+
     private fun initUI()
     {
         GuiHelper.setTextViewPatternBackground(resources,R.drawable.pattern,binding!!.mainFragmentHeaderTextView)
@@ -37,6 +53,71 @@ class MainFragment : Fragment() {
 
     private fun init()
     {
+        val filter = IntentFilter(BroadcastReceiverHelper.NotificationServiceBroadcastReceiver)
+        context?.registerReceiver(receiver, filter)
+        muteStateAction = MuteStateActionHelper.getMuteStateAction(requireContext())
+    }
+
+    private val switchChange = object : CompoundButton.OnCheckedChangeListener
+    {
+        override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean)
+        {
+            if (::muteStateAction.isInitialized)
+                muteStateAction.switchMuteState()
+        }
+    }
+
+    private val receiver = object : NotificationServiceBroadcastReceiver()
+    {
+        override fun onReceive(p0: Context?, p1: Intent?)
+        {
+            super.onReceive(p0, p1)
+            val state = p1!!.getBooleanExtra(MuteStateSharedPrefAction.MUTE_STATE_KEY,false)
+            checkNotificationState(state)
+        }
+    }
+
+    private fun checkNotificationState(state:Boolean)
+    {
+        binding!!.mainFragmentMuteSwitch.setOnCheckedChangeListener(null)
+        binding!!.mainFragmentMuteSwitch.isChecked = state
+        binding!!.mainFragmentMuteSwitch.setOnCheckedChangeListener(switchChange)
+        setState(state)
+    }
+
+    private  fun setStateInit()
+    {
+        val state = MuteStateActionHelper.getMuteStateAction(requireContext()).getMuteState()
+        checkNotificationState(state)
+    }
+
+    private  fun setState(state:Boolean)
+    {
+        binding!!.mainFragmentMuteStateTextView.apply {
+            if(state)
+            {
+                text = getString(R.string.mute)
+                setTextColor(ContextCompat.getColor(context, R.color.colorMuteSetState))
+            }
+            else
+            {
+                text = getString(R.string.notification)
+                setTextColor(ContextCompat.getColor(context, R.color.colorNotificationSetState))
+            }
+
+        }
+
+        binding!!.mainFragmentMuteStateExpTextView.apply {
+
+            val sp : Spanned = when(state)
+            {
+                true -> HtmlCompat.fromHtml(getString(R.string.muteExp), HtmlCompat.FROM_HTML_MODE_LEGACY)
+                else -> HtmlCompat.fromHtml(getString(R.string.notificationExp), HtmlCompat.FROM_HTML_MODE_LEGACY)
+            }
+
+            text = sp
+
+        }
 
     }
 
